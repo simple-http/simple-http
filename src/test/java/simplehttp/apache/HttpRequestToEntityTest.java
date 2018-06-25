0ac4c89debb2dcc3330de62a8957590c2618f49c
Toby <toby.weston@gmail.com>
2018-06-25 20:41:49 +0100

    closes #15

--- src/test/java/simplehttp/apache/HttpRequestToEntityTest.java
+++ src/test/java/simplehttp/apache/HttpRequestToEntityTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013, bad robot (london) ltd
+ * Copyright (c) 2011-2018, simple-http committers
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
