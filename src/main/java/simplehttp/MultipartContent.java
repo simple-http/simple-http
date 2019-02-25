daadd02838762bbf042c56adbd24cfd8f945a7db
Toby <toby.weston@gmail.com>
2019-02-25 21:50:19 +0000

    #7 make message content public!

--- src/main/java/simplehttp/MultipartContent.java
+++ src/main/java/simplehttp/MultipartContent.java
@@ -31,7 +31,7 @@ import java.util.Objects;
  * actual entity and create a dynamic boundary, differing each time).
  * 
  */
-class MultipartContent implements MessageContent {
+public class MultipartContent implements MessageContent {
 	
 	private final String name;
 	private final File file;
