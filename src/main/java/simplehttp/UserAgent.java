f9a34d54c65f9f136624753a09acf2e13f952be2
Toby <toby.weston@gmail.com>
2019-02-25 22:08:26 +0000

    #19 more nonsense

--- src/main/java/simplehttp/UserAgent.java
+++ src/main/java/simplehttp/UserAgent.java
@@ -25,8 +25,8 @@ import static simplehttp.HeaderPair.header;
 
 public interface UserAgent {
 
-    public final static Header Apache = header("User-Agent", "Apache-HttpClient/4.1.3 (java 1.5)");
-    public final static Header SafariOnMac = header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.17 (KHTML, like Gecko) Version/6.0.2 Safari/536.26.17");
-    public final static Header ChromeOnMac = header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+    Header Apache = header("User-Agent", "Apache-HttpClient/4.1.3 (java 1.5)");
+    Header SafariOnMac = header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.17 (KHTML, like Gecko) Version/6.0.2 Safari/536.26.17");
+    Header ChromeOnMac = header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 
 }
